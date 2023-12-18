465281165601857cda6fbe36276ef07f6fd0ffc2
Hazel <rosalvahmartinez@gmail.com>
2023-12-17 20:36:18 -0600

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,11 @@
+public class Product 
+{
+    public int Id { get; set; }
+    public string Productname { get; set; }
+    public string Description { get; set; }
+    public string Material { get; set; }
+    public string Size { get; set; }
+    public string Color { get; set; }
+     public decimal Price { get; set; }
+     public int StockQuantity { get; set; }
+};
